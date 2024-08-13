87f4fefcf5df3dec23d8373d052e6db8ee4753cb
daichanghong <daichanghong@jiuqi.com.cn>
2024-08-13 15:03:10 +0800

    增加cpu信息解析

--- webserver/src/models/cpu.rs
+++ webserver/src/models/cpu.rs
@@ -1,8 +1,9 @@
 pub struct Cpu {
+    time: String,
+    running_time: u64,
     us: f64,
     sy: f64,
     id: f64,
-    mem: f64,
     tasks: u32,
     running: u32,
     sleeping: u32,
@@ -13,21 +14,41 @@ pub struct Cpu {
 }
 
 impl Cpu {
-    pub fn new(line: &str) -> Self {
+    pub fn new(lines: Vec<&str>) -> Self {
+      let (time, running_time) = Self::extract_cpu(lines.get(0).unwrap());
+      let (tasks, running, sleeping) = Self::extract_task(lines.get(1).unwrap());
         Cpu {
+            time,
+            running_time,
             us: todo!(),
             sy: todo!(),
             id: todo!(),
-            mem: todo!(),
-            tasks: todo!(),
-            running: todo!(),
-            sleeping: todo!(),
+            tasks,
+            running,
+            sleeping,
             mem_total: todo!(),
             mem_free: todo!(),
             mem_used: todo!(),
             process: todo!(),
         }
     }
+    fn extract_cpu(line: &str) -> (String, u64) {
+        let infos: Vec<&str> = line.split(",").collect();
+        let cpu_running:Vec<&str> = infos[1].split_whitespace().collect();
+        (cpu_running[2].to_string(), u64::from_str_radix(cpu_running[4], 16).unwrap())
+    }
+
+    fn extract_task(line: &str) -> (u32, u32, u32){
+      let tasks:Vec<&str> = line.split(",").collect();
+      let total_info:Vec<&str> = tasks.get(0).unwrap().split_whitespace().collect();
+      let running_info:Vec<&str> = tasks.get(1).unwrap().split_whitespace().collect();
+      let sleep_info:Vec<&str> = tasks.get(2).unwrap().split_whitespace().collect();
+      let total = total_info.get(1).unwrap().parse::<u32>().unwrap();
+      let running = running_info.get(0).unwrap().parse::<u32>().unwrap();
+      let sleep = sleep_info.get(0).unwrap().parse::<u32>().unwrap();
+      (total, running, sleep)
+    }
+
 }
 
 pub struct Process {
@@ -39,16 +60,15 @@ pub struct Process {
     command: String,
 }
 
-
-impl Process{
-  pub fn new(lines: Vec<&str>) -> Self{
-    Process{
-        pid: todo!(),
-        usr: todo!(),
-        cpu: todo!(),
-        mem: todo!(),
-        time: todo!(),
-        command: todo!(),
+impl Process {
+    pub fn new(lines: Vec<&str>) -> Self {
+        Process {
+            pid: todo!(),
+            usr: todo!(),
+            cpu: todo!(),
+            mem: todo!(),
+            time: todo!(),
+            command: todo!(),
+        }
     }
-  }
 }
